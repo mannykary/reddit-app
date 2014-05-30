2ca5683b82d7c6cafda6d102b897f0a6e999a3b5
mannykary <mrkaryam@gmail.com>
2014-05-29 22:46:17 -0700

    displays posts, thumbnails, scores

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
+require 'net/http'
+require 'json'
+
 class StaticPagesController < ApplicationController
   def about
+  end
+
+  def home
+    url = 'http://www.reddit.com/r/all.json'
+    request = Net::HTTP.get(URI.parse(url.to_s))
+    hash = JSON.parse request
+    @subhash = hash["data"]["children"]
   end
 end

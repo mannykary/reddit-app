5510015a6f8842e294cfc2f5657c92ab6b101a29
mannykary <mrkaryam@gmail.com>
2014-06-10 21:40:59 -0700

    next page link now worksgit status

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,27 @@ require 'json'
 require 'date'
 
 class StaticPagesController < ApplicationController
+  
+  BASE_URL = 'http://www.reddit.com/r/all.json?limit=100'
+  
   def about
   end
 
   def home
-    url = 'http://www.reddit.com/r/all.json'
-    request = Net::HTTP.get(URI.parse(url.to_s))
-    hash = JSON.parse request
-    @subhash = hash["data"]["children"]
+    if params[:after] != nil
+      url = BASE_URL + '&after=' + params[:after].to_s
+    else
+      url = BASE_URL
+    end
+    
+    parse_json url
+  
   end
+    
+  def parse_json (url)
+    request = Net::HTTP.get(URI.parse(url.to_s))
+    @hash = JSON.parse request
+    @subhash = @hash["data"]["children"]
+  end
+  
 end
